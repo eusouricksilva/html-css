f660ad3bcf769fa4bc6ed6114252e3a160457d0c
Pedro Henrique <pedrohenriquedasilvacontato@gmail.com>
2023-03-29 14:53:54 -0300

    atualização

--- exercicios/ex025/cadastro.php
+++ exercicios/ex025/cadastro.php
@@ -1,2 +1,15 @@
-Os dados foram enviados!
-Aprenda PHP para saber o que fazer com eles.
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Cadastro</title>
+</head>
+<body>
+	<h1>Seja Bem-Vindo!</h1>
+	<?php
+		// Este é um exemplo de código PHP
+		$mensagem = "Os dados foram enviados! Aprenda PHP para saber o que fazer com eles.";
+		echo $mensagem;
+	?>
+</body>
+</html>
+
